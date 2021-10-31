e5199347229dd188ff14943f7495628d9366d106
Khrove <joshualjohnson33@gmail.com>
2021-10-31 16:24:10 -0400

    Update account.ts

--- greatminds/data/dashboard/production/account.ts
+++ greatminds/data/dashboard/production/account.ts
@@ -1,22 +1,8 @@
 export const accounts = {
-    teacher: {
-        username: "dd-tone@yopmail.com",
-        password: "Test@123"
-    },
-    student: {
-        username: "dd-sone@yopmail.com",
-        password: "Test@123"
-    },
-    districtAdmin: {
-        username: "dd-daone@yopmail.com",
-        password: "Test@123"
-    },
-    siteCode: {
-        code: "DemoDigital",
-        usertype: "District Administrator",
-        username: "mr.four",
-        password: "Test@123"
-    },
+    teacher: {},
+    student: {},
+    districtAdmin: {},
+    siteCode: {},
     visual: {
         password: 'BeepBoopRoboot'
     },
